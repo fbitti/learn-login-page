b19ffe9e2f08bb3e3f5b1bafac0d0a69c5b26981
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-02 17:12:00 +0200

    try --- catch to handle exceptions

--- resource/Database.php
+++ resource/Database.php
@@ -1,7 +1,15 @@
 <?php
 
-$db = new PDO('mysql:host=' . $_SERVER['DB_HOST'] .'; dbname=' . $_SERVER['DB_DATABASE'], $_SERVER['DB_USERNAME'], $_SERVER['DB_PASSWORD']);
-echo "Connected to the register database\n";
+// DSN =  Data Source Name
+$dsn = 'mysql:host=' . $_SERVER['DB_HOST'] .'; dbname=' . $_SERVER['DB_DATABASE'];
+$db_username = $_SERVER['DB_USERNAME'];
+$db_password = $_SERVER['DB_PASSWORD'];
 
+try {
+  $db = new PDO($dsn, $db_username, $db_password);
+  echo "Connected to the register database\n";
+} catch (PDOException $exception) {
+  echo "The database connection failed" . $exception->getMessage() . "\n";
+}
 
 ?>

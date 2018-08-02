9453720572b29b08d52a3731a1ee589b16499f29
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-02 17:43:36 +0200

    registration page added

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
   <table>
       <tr><td>Username:</td> <td><input type="text" value=""></td></tr>
       <tr><td>Password:</td> <td><input type="password" value=""></td></tr>
-      <tr><td></td> <td><input type="submit" value="Signin"></td></tr>
+      <tr><td></td> <td><input style="float:right;" type="submit" value="Signin"></td></tr>
   </table>
 
 <p> <a href="index.php">Back</a> </p>

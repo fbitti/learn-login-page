3a4d186aeafa1a0f7f78af8304b945bcc9f4b1d4
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-02 17:29:36 +0200

    first index.php page

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 <body>
 <h2>User Authentication System </h2><hr>
 
-<?php include_once 'resource/Database.php' ?>
+<p>You are currently not signed in.<a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </p>
+
+<p>You are logged in as {username} <a href="logout.php">Logout</a> </p> 
+
 </body>
 </html>

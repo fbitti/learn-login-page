0487efde9e4fcb3370f16372ef5ded6bdb7e070e
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-02 17:47:26 +0200

    signup page $_POST

--- signup.php
+++ signup.php
@@ -9,11 +9,15 @@
 
 <h3> Registration Form </h3>
 
+<?php
+var_dump($_POST);
+?>
+
 <form method="post" action="">
   <table>
-      <tr><td>Email:</td> <td><input type="text" value=""></td></tr>
-      <tr><td>Username:</td> <td><input type="text" value=""></td></tr>
-      <tr><td>Password:</td> <td><input type="password" value=""></td></tr>
+      <tr><td>Email:</td> <td><input type="text" value="" name="email"></td></tr>
+      <tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
+      <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
       <tr><td></td> <td><input style="float:right;" type="submit" value="Sign Up"></td></tr>
   </table>
 

f1a597c84b22952791a5370d2d588b7d6a3b1ef1
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-05 08:14:30 +0200

    add session to login

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+include_once "resource/session.php";
+include_once "resource/Database.php";
+include_once "resource/utilities.php";
+
+if (isset($_POST["loginBtn"]) {
+
+}
+
+// array to store the errors
+$form_errors = array();
+
+// validate
+$required_fields = array('username', 'password');
+$form_errors = check_empty_fields($required_fields);
+
+if (empty($form_errors)) {
+
+} else {    // ! empty($form_errors)
+  if (count($form_errors == 1)) {
+    $formErrorHTML = "<p style='color: red;'>There is one error in the form.</p>";
+  } else {    // ! count($form_errors == 1)
+    $formErrorHTML = "<p style='color: red;'>There were " . count($form_errors) . " errors in the form.</p>";
+  }
+}
+
+ ?>
+
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -9,11 +38,13 @@
 
 <h3> Login Form </h3>
 
+<?php if(isset($formErrorHTML)) echo $formErrorHTML; ?>
+<?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
 <form method="post" action="">
   <table>
-      <tr><td>Username:</td> <td><input type="text" value=""></td></tr>
-      <tr><td>Password:</td> <td><input type="password" value=""></td></tr>
-      <tr><td></td> <td><input style="float:right;" type="submit" value="Signin"></td></tr>
+      <tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
+      <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
+      <tr><td></td> <td><input style="float:right;" type="submit" value="Signin" name="loginBtn"></td></tr>
   </table>
 
 <p> <a href="index.php">Back</a> </p>

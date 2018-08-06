a45de5afd55491ed0ac8deed409e7a73bd92d7ab
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-06 07:33:48 -0400

    show the username if logged in

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include_once "resource/session.php";
 <?php if( !isset($_SESSION['username']) ): ?>
 <p>You are currently not signed in. <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </p>
 <?php else: ?>
-<p>You are logged in as {username} <a href="logout.php">Logout</a> </p>
+<p>You are logged in as <?php if(isset($_SESSION)) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a> </p>
 <?php endif ?>
 </body>
 </html>

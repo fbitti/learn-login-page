c0c30562a9bc78c04e21414ef5b0c8f0db224137
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-09 05:37:55 -0400

    Revert "add another <br>"
    This reverts commit e47fb789759f8155415efb7ed4757185df014bd1.

--- index.php
+++ index.php
@@ -53,7 +53,11 @@ include_once "resource/session.php";
   <main role="main" class="container">
 
     <div class="starter-template">
+<<<<<<< HEAD
       <br><br><br>
+=======
+      <br>
+>>>>>>> parent of e47fb78... add another <br>
       <h1>Bootstrap starter template</h1>
       <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
     </div>

1be78ee69cf7b618741ae41eb6a292c57496e7fe
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-09 12:27:21 -0400

    align center

--- index.php
+++ index.php
@@ -52,8 +52,7 @@ include_once "resource/session.php";
 
   <main role="main" class="container">
 
-    <div class="starter-template">
-      <br><br>
+    <div class="flag">
       <h1>Bootstrap starter template</h1>
       <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
     </div>

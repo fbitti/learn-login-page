d4773ea36fbeb38e38d3b258e2fa58193a928160
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-10 09:44:20 -0400

    continue to debug highlighting

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,14 @@
 var pageUrl = window.location;
 
-$('ul.nav-item a').filter(function() {
+$('ul.navbar-nav.mr-auto a').filter(function() {
   return this.href == pageUrl;
+
+  console.log(this.href, this.href == pageUrl);
+
 }).parent().addClass('active');
 
-/* // from https://codepen.io/gearmobile/pen/bByZdG
-$( '.navbar-nav a' ).on( 'click', function () {
-	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+// from https://codepen.io/gearmobile/pen/bByZdG
+/* $( 'ul.navbar-nav.mr-auto a' ).on( 'click', function () {
+	$( 'ul.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 }); */

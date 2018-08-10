b56a008a78f26cbd3d76cce583a38de5c515a5c8
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-10 09:13:43 -0400

    try to make it work again

--- partials/headers.php
+++ partials/headers.php
@@ -18,15 +18,15 @@ include_once "resource/session.php";
 <body>
 
   <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="index.php">User Authentication</a>
+    <span class="navbar-brand">User Authentication</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
         </li>
 
         <?php if(!isset($_SESSION['username']) ): ?>

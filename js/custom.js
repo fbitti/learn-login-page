b2052d22992539bb1aa0a295b234b6e699e5aa31
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-10 09:02:43 -0400

    fix variable name

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
-var pageUrl = window.location;
+var pageUrl = window.location.href;
 
-/* $('ul.nav a').filter(function() {
-  return this.href == url;
-}).parent().addClass('active'); */
+$('ul.nav a').filter(function() {
+  return this.href == pageUrl;
+}).parent().addClass('active');
 
-// from https://codepen.io/gearmobile/pen/bByZdG
+/* // from https://codepen.io/gearmobile/pen/bByZdG
 $( '.navbar-nav a' ).on( 'click', function () {
 	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
-});
+}); */

7a64b44996cddf181ad7466ff5818f281e5d44f7
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-10 08:56:21 -0400

    another approach to fix active selection highlighting on the menu

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 var pageUrl = window.location;
 
-$('ul.nav a').filter(function() {
+/* $('ul.nav a').filter(function() {
   return this.href == url;
-}).parent().addClass('active');
+}).parent().addClass('active'); */
+
+// from https://codepen.io/gearmobile/pen/bByZdG
+$( '.navbar-nav a' ).on( 'click', function () {
+	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});

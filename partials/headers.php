fe2660f6bce86a877f35ecab46bff80d27a75d95
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-08-10 09:46:30 -0400

    add a link back to the navbar brand

--- partials/headers.php
+++ partials/headers.php
@@ -18,7 +18,7 @@ include_once "resource/session.php";
 <body>
 
   <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">User Authentication</a>
+    <a class="navbar-brand" href="index.php">User Authentication</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
